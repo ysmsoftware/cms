bcc1b027b15041545c62c9137d040b7c1d764fca
Rushikesh Patil <126864321+hrishikesh62435@users.noreply.github.com>
2024-03-07 18:46:00 +0530

    second commit

--- cms/src/main/java/com/cms/CmsApplication.java
+++ cms/src/main/java/com/cms/CmsApplication.java
@@ -2,12 +2,19 @@ package com.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @SpringBootApplication
 public class CmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
+		System.out.println("ggg");
+		
 	}
 
 }
