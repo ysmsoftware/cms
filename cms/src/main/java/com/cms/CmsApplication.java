f3f351b0712a29612c982dbcbbc7b3c6ba54617e
PC-1 <PC-1@192.168.1.11>
2024-03-08 10:56:49 +0530

    commit from vaishnavi

--- cms/src/main/java/com/cms/CmsApplication.java
+++ cms/src/main/java/com/cms/CmsApplication.java
@@ -13,7 +13,7 @@ public class CmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
-		System.out.println("hello practice");
+		System.out.println("hi practice");
 		
 	}
 
