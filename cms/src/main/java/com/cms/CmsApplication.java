cf71d9a60d692e068e6becf51d75923b2a164ac2
PC-1 <PC-1@192.168.1.10>
2024-03-20 11:13:13 +0530

    commit from vaishnavi

--- cms/src/main/java/com/cms/CmsApplication.java
+++ cms/src/main/java/com/cms/CmsApplication.java
@@ -2,6 +2,9 @@ package com.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,7 +16,7 @@ public class CmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
-		System.out.println("vaishanvi");
+		System.out.println("vaishnavi");
 		
 	}
 
--- cms/src/main/java/com/cms/SwaggerConfig.java
+++ cms/src/main/java/com/cms/SwaggerConfig.java
@@ -1,17 +1,25 @@
 package com.cms;
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 
 @Configuration
 @EnableSwagger2
+@EnableTransactionManagement
 public class SwaggerConfig {
 
 	
@@ -30,4 +38,10 @@ public class SwaggerConfig {
 		 
 	  
 			}
+	
+	
+	
+	
+	
+
 }
